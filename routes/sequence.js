1e70ac8a23f4f2e0aeed532b1c3dabf437a1f380
nchlab <bt17684@qmul.ac.uk>
2018-07-19 19:20:20 +0100

    Comments added

--- routes/sequence.js
+++ routes/sequence.js
@@ -6,11 +6,13 @@ const mongoose = require("mongoose");
 router.get("/:id", (req, res) => {
   console.log("Connection |", "Method:", req.method + " |", "URL:", req.get('host') + req.originalUrl);
 
+// If user clicks on /Sequence, by Default ORF1p sequence returned > can automate this to grab data from db
   if ((req.originalUrl == "/sequence/undefined") || (req.originalUrl == "/sequence")) {
     // mongoose.model("seqview1").find({"Family":""}, function(err, posts) {
       res.json({"Family":"ORF1p",
                 "Sequence":"MGKKQNRKTGNSKTQSASPPPKERSSSPATEQSWMENDFDELREEGFRRSNYSELREDIQTKGKEVENFEKNLEECITRITNTEKCLKELMELKTKARELREECRSLRSRCDQLEERVSAMEDEMNEMKREGKFREKRIKRNEQSLQEIWDYVKRPNLRLIGVPESDVENGTKLENTLQDIIQENFPNLARQANVQIQEIQRTPQRYSSRRATPRHIIVRFTKVEMKEKMLRAAREKGRVTLKGKPIRLTADLSAETLQARREWGPIFNILKEKNFQPRISYPAKLSFISEGEIKYFIDKQMLRDFVTTRPALKELLKEALNMERNNRYQPLQNHAKM"});
   } else {
+    // Removes /sequence/ from the family_name
     var fam_name = req.originalUrl.slice(10);
     mongoose.model("seqview1").find({"Family":fam_name}, function(err, posts) {
       res.json(posts);
