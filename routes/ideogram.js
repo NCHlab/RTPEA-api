ae321e9a08d5bcc0ca41b5ab32eca353c987e839
nchlab <bt17684@qmul.ac.uk>
2018-07-19 19:26:48 +0100

    Commenting of code + removal of old code

--- routes/ideogram.js
+++ routes/ideogram.js
@@ -5,55 +5,52 @@ const mongoose = require("mongoose");
 
 router.get("/:id", (req, res) => {
   console.log("Connection |", "Method:", req.method + " |", "URL:", req.get('host') + req.originalUrl);
-  // console.log(req.originalUrl)
+// If user clicks on /ideogram, by Default ALL the data is returned
   if ((req.originalUrl == "/ideogram/undefined") || (req.originalUrl == "/ideogram")) {
     mongoose.model("ideogram1").find({}, function(err, posts) {
       res.json(posts);
     });
   } else {
+    // Regex identifies all the family names seperated by comma or space
+    // Saves the data to data_name followed by further parsing
+    // depending on the type of naming convention used e.g HS_101 or LINE_1_HS_101
     const regex = /[A-Z]+\d?_+\d+_?[A-Z]*\d*_*\d*[A-Z]*/gm;
     const str = req.originalUrl;
     let m;
     var data_name = [];
     var family_name = [];
-    var text_string=[];
-
+    var text_string = [];
 
     while ((m = regex.exec(str)) !== null) {
-        // This is necessary to avoid infinite loops with zero-width matches
-        if (m.index === regex.lastIndex) {
-            regex.lastIndex++;
-        }
+      // This is necessary to avoid infinite loops with zero-width matches
+      if (m.index === regex.lastIndex) {
+        regex.lastIndex++;
+      }
 
-        // The result can be accessed through the `m`-variable.
-        m.forEach((match, groupIndex) => {
-            // console.log(`${match}`);
-            data_name.push(match)
-
-        });
+      // The result can be accessed through the `m`-variable.
+      m.forEach((match, groupIndex) => {
+        data_name.push(match);
+      });
     }
     var i;
     for (i = 0; i < data_name.length; i++) {
       // Checks for name such as LINE_1_HS_101_ORF1
-        // if (data_name[i].slice(-5,-4) == "_"){
-        //   family_name.push(data_name[i].slice(0,-5))
-
+      // if (data_name[i].slice(-5,-4) == "_"){
+      //   family_name.push(data_name[i].slice(0,-5))
       // Checks for name such as LINE_1_HS_101
-        // } else
-        if (data_name[i].slice(0,4) == "LINE"){
-          family_name.push(data_name[i])
-        } else{
-      // Name such as HS_101
-          family_name.push("LINE_1_"+data_name[i])
-        }
-    }
-    // db.getCollection('ideogram1').find( { $or: [ {"name":"LINE_1_HS_101" }, { "name":"LINE_1_HS_103"}]})
-
-
-      var j;
-      for (j = 0; j < family_name.length; j++) {
-        text_string.push({"name":family_name[j]})
+      // } else
+      if (data_name[i].slice(0, 4) == "LINE") {
+        family_name.push(data_name[i]);
+      } else {
+        // Name such as HS_101
+        family_name.push("LINE_1_" + data_name[i]);
       }
+    }
+
+    var j;
+    for (j = 0; j < family_name.length; j++) {
+      text_string.push({ name: family_name[j] });
+    }
 
     mongoose.model("ideogram1").find({$or:text_string}, function(err, posts) {
       res.json(posts);
@@ -63,6 +60,7 @@ router.get("/:id", (req, res) => {
 
 module.exports = router;
 
+//OLD CODE
 // mongoose.model("ideogram1").find({$or:[{"name":family_name[0]},
 //                                         {"name":family_name[1]},
 //                                         {"name":family_name[2]},
