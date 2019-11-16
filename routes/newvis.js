7d3f2cc2c469f780801afdfaa9eb759268518972
nchlab <bt17684@qmul.ac.uk>
2019-11-16 16:38:31 +0000

    Added PTM code, displays PTMs for selected tissue, + removed old code

--- routes/newvis.js
+++ routes/newvis.js
@@ -6,32 +6,6 @@ const mongoose = require("mongoose");
 router.get("/:pxdid/:tissue/:state/:siftScore/", (req, res) => {
   console.log("Connection |", "Method:", req.method + " |", "URL:", req.originalUrl);
 
- // console.log("Connection |", "Method:", req.method + " |", "URL:",  req.get('host') + req.originalUrl);
-  // const Error_404_msg = {
-  //   Status: "Not Found",
-  //   Code: 404,
-  //   Message:  " does not exist in the database.",
-  //   moreInfoUrl: "http://www.rtpea.com/status/404"
-  // };
-  //console.log(req.params.state)
-  //var PXD_ID = req.params.pxd.toUpperCase();
-  //if (PXD_ID == "UNDEFINED"){
-  //  // PXD_ID = "ORF1P"
-  //  PXD_ID = "ORF1P"
-  //} else if (PXD_ID == "ORF1") {
-  //    PXD_ID = PXD_ID +"P"
-  //} else if (PXD_ID == "ORF2") {
-  //  PXD_ID = PXD_ID +"P"
-  //}
-  //
-  //console.log(req.params.pxdid)
-  //console.log(req.params.tissue)
-  //console.log(req.params.state)
-  //console.log(req.params.state.replace("search",""))
-  
-  //req.params.state = req.params.state.replace("search","")
-  // SORT THE REQ PARAMS WHEN MULTIPLE SELECTIONS
-   
   state = req.params.state.replace("search","").replace("undefined","")
    
    
@@ -40,19 +14,9 @@ router.get("/:pxdid/:tissue/:state/:siftScore/", (req, res) => {
     var healthyData = []
     var diseasedData = []
     var bothData = []
-    //console.log(posts[0].features[0].consequence3)
 
-    //console.log("test")
     posts[0].features.forEach(function(features) {
-        //console.log(features.consequence3);
-        
-              
-            //  "consequence" : "liver",
-            //"consequence2" : "",
-            //"consequence3" : "meta_missing",
-            //
-            
-        // PTM CODE
+
         
         if (features.category === "PTM" && features.tissue === req.params.tissue){
           healthyData.push(features)
@@ -98,11 +62,9 @@ router.get("/:pxdid/:tissue/:state/:siftScore/", (req, res) => {
       
     }
     res.json(posts);
-    //res.json(listOfData);
 
   });
   
-  //return res.json(req.params.state)
 });
 
 
