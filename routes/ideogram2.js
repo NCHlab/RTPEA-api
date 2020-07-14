2867bb3d05cc8745aef5772d94c29727c4f64c8e
nchlab <bt17684@qmul.ac.uk>
2020-07-15 00:27:02 +0100

    Backend Logic for New chromosome format

--- routes/ideogram2.js
+++ routes/ideogram2.js
@@ -6,22 +6,38 @@ const mongoose = require("mongoose");
 router.get("/:id", (req, res) => {
   console.log("Connection |", "Method:", req.method + " |", "URL:", req.get('host') + req.originalUrl);
 
-    mongoose.model("ideogram122").find({}, function(err, posts) {
-      //delete posts[0]._id
-      //console.log(posts[0]['annotations'])
-      res.json(posts[0]['annotations']);
+  if ((req.originalUrl == "/ideogram2/undefined") || (req.originalUrl == "/ideogram2")) {
+    mongoose.model("ideogram122").find({}, function (err, posts) {
+      res.json(posts[0]["annotations"]);
     });
-  });
+  } else {
+    const regex = /[A-Z]+\d?_+\d+_?[A-Z]*\d*_*\d*[A-Z]*/gm;
+    const str = req.originalUrl;
+    let m;
+    var data_name = [];
+
+    while ((m = regex.exec(str)) !== null) {
+      // This is necessary to avoid infinite loops with zero-width matches
+      if (m.index === regex.lastIndex) {
+        regex.lastIndex++;
+      }
+      // The result can be accessed through the `m`-variable.
+      m.forEach((match, groupIndex) => {
+        data_name.push(match);
+      });
+    }
+
+    var data_to_send = [];
+
+    mongoose.model("ideogram122").find({}, function (err, posts) {
+      for (var j of posts[0]["annotations"]) {
+        if (data_name.includes(j["name"])) {
+          data_to_send.push(j);
+        }
+      }
+      res.json(data_to_send);
+    });
+  }
+});
 
 module.exports = router;
-
-//OLD CODE
-// mongoose.model("ideogram1").find({$or:[{"name":family_name[0]},
-//                                         {"name":family_name[1]},
-//                                         {"name":family_name[2]},
-//                                         {"name":family_name[3]},
-//                                         {"name":family_name[4]},
-//                                         {"name":family_name[5]},
-//                                         {"name":family_name[6]}]}, function(err, posts) {
-//   res.json(posts);
-// });
