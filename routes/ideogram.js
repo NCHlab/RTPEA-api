dbe7a4e6fca1e1d8dba8bfcd8a03927c84fea5e0
nchlab <bt17684@qmul.ac.uk>
2020-07-15 00:43:17 +0100

    removing old commented code

--- routes/ideogram.js
+++ routes/ideogram.js
@@ -59,14 +59,3 @@ router.get("/:id", (req, res) => {
   });
 
 module.exports = router;
-
-//OLD CODE
-// mongoose.model("ideogram1").find({$or:[{"name":family_name[0]},
-//                                         {"name":family_name[1]},
-//                                         {"name":family_name[2]},
-//                                         {"name":family_name[3]},
-//                                         {"name":family_name[4]},
-//                                         {"name":family_name[5]},
-//                                         {"name":family_name[6]}]}, function(err, posts) {
-//   res.json(posts);
-// });
